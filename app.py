8cbe895964c0861243b22ab1e158ac5dadf04375
TheWeisGuy <henryweisman14@gmail.com>
2021-11-02 19:32:45 -0400

    Testing form.html

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/") 
 def index():
-    return "Welcome to the App in bare-bone state!"
+    return ("Welcome to the App in bare-bone state!",render_template("form.html"))
 
 
 if __name__ == "__main__":
