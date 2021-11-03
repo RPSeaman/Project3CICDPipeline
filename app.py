c18c1d56147c22d5d69b55a2d57994c381d5b3e5
TheWeisGuy <henryweisman14@gmail.com>
2021-11-02 20:25:53 -0400

    Added the add method to app

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
 
 app = Flask(__name__)
 
+tasks = []
 
 @app.route("/") 
 def index():
-    return render_template("form.html")
+    return render_template("form.html" ,tasks = tasks)
 
+@app.route("/add",methods=["POST"])
+def add():
+    now  = datetime.now()
+    
+    task = request.form.get("task")
+    tasks.append(task)
+    print(tasks[-1])
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug = True)
