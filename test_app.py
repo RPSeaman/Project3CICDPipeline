27ceed274cb1f6745e0cd1631797f0540340ea70
RPSeaman <rpseam22@colby.edu>
2021-11-02 20:33:00 -0400

    Fixed Tests

--- test_app.py
+++ test_app.py
@@ -1,6 +1,13 @@
 from app import *
 #ds
 
+# websitePath = https://rh-to-do-list.herokuapp.com
+
+# websitePath = http://127.0.0.1:5000
+
+
 def test_index():
-    assert index() == "Welcome to the App in bare-bone state!"
- 
+    
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
