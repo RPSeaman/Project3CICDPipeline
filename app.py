69adaec18837ecd1fc99fa0b3cd1670678720a84
TheWeisGuy <henryweisman14@gmail.com>
2021-11-08 19:29:11 -0500

    Added remove method

--- app.py
+++ app.py
@@ -7,17 +7,26 @@ tasks =[]
 
 @app.route("/") 
 def index():
-    return render_template("form.html")
+    return render_template("form.html",tasks = tasks)
 
-@app.route("/add")
+@app.route("/add", methods=["POST"])
 def add():
-    title = request.form.get("visitor")
+
+    
+    title = request.form.get("task")
     print(title)
     newTask = task.Task(title)
-    tasks.append(newTask)
-    print(tasks[-1].getTitle()," ",tasks[-1].getDate())
+    tasks.append(title)
+    #print(tasks[-1].getTitle()," ",tasks[-1].getDate())
 
     return redirect(url_for("index"))
+
+
+@app.route("/remove/<string:name>")
+def remove(name):
+    tasks.remove(name)
+    return redirect(url_for("index"))
+    
 if __name__ == "__main__":
     app.run(debug = True)
  
