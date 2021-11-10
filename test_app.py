68b6f4100e0ace4b95426b811cf78e1bbe2b9537
TheWeisGuy <henryweisman14@gmail.com>
2021-11-09 20:18:13 -0500

    Reformatted with Black

--- test_app.py
+++ test_app.py
@@ -65,48 +65,48 @@ def test_addTag():
     response = client.post(url, data=newTag)
     response = client.get("/")
     webpage_text = response.get_data()
-    assert (b'testTag' in webpage_text)
-    
-def test_changeStatus():
-    # change task
-    client = app.test_client()
-    response = client.get("/")
-    webpate_text = response.get_data()
-    url = '/add'
-    newTask = {'task':'test'}
-    response = client.post(url, data=newTask)
-    assert response.status_code == 302
-    response = client.get("/")
-    webpage_text = response.get_data()
-    assert b'test' in webpage_text
-    
-    # change status and test
-    url = '/updateStatus/Started'
-    response = client.post(url, data=newTask)
-    assert response.status_code == 302
-    response = client.get("/")
-    webpage_text = response.get_data()
-    assert (b'Started' in webpage_text)
-    
-def test_changeStatus():
-    # change task
-    client = app.test_client()
-    response = client.get("/")
-    webpate_text = response.get_data()
-    url = '/add'
-    newTask = {'task':'test'}
-    response = client.post(url, data=newTask)
-    assert response.status_code == 302
-    response = client.get("/")
-    webpage_text = response.get_data()
-    assert b'test' in webpage_text
-    
-    # change priority and test
-    url = '/updatePriority/High'
-    response = client.post(url, data=newTask)
-    assert response.status_code == 302
-    response = client.get("/")
-    webpage_text = response.get_data()
-    assert (b'High' in webpage_text)
+    assert b"testTag" in webpage_text
 
-    
+
+def test_changeStatus():
+    # change task
+    client = app.test_client()
+    response = client.get("/")
+    webpate_text = response.get_data()
+    url = "/add"
+    newTask = {"task": "test"}
+    response = client.post(url, data=newTask)
+    assert response.status_code == 302
+    response = client.get("/")
+    webpage_text = response.get_data()
+    assert b"test" in webpage_text
+
+    # change status and test
+    url = "/updateStatus/Started"
+    response = client.post(url, data=newTask)
+    assert response.status_code == 302
+    response = client.get("/")
+    webpage_text = response.get_data()
+    assert b"Started" in webpage_text
+
+
+def test_changeStatus():
+    # change task
+    client = app.test_client()
+    response = client.get("/")
+    webpate_text = response.get_data()
+    url = "/add"
+    newTask = {"task": "test"}
+    response = client.post(url, data=newTask)
+    assert response.status_code == 302
+    response = client.get("/")
+    webpage_text = response.get_data()
+    assert b"test" in webpage_text
+
+    # change priority and test
+    url = "/updatePriority/High"
+    response = client.post(url, data=newTask)
+    assert response.status_code == 302
+    response = client.get("/")
+    webpage_text = response.get_data()
+    assert b"High" in webpage_text
